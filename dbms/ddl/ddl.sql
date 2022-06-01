87fd0d66b5e208d8ae345dcca4c4a5b9e72a3349
feret.jocelin <feret.jocelin@outlook.fr>
2022-06-01 14:28:09 +0200

    bd

--- dbms/ddl/ddl.sql
+++ dbms/ddl/ddl.sql
@@ -3,9 +3,11 @@
 create table if not exists user (
   userId int auto_increment not null,
   email varchar(256) unique not null, 
-  familyName varchar(256), 
-  givenName varchar(256), 
-  primary key(userId)
+  firstName varchar(256), 
+  lastName varchar(256),
+  roleId varchar(256) not null,
+  primary key(userId),
+  foreign key(roleId) references role(roleId)
 );
 
 
@@ -16,11 +18,51 @@ before insert
 on user for each row set new.email = lower(trim(new.email));
 
 
-/* exemple */
-create table if not exists film (
-  filmId int auto_increment not null,
-  title varchar(256) not null, 
-  duration int default 0,
-  rentalRate decimal(6,2) not null default 0 check (rentalRate >= 0),
-  primary key(filmId)
+/* challenge */
+create table if not exists challenge (
+  challengeId int auto_increment not null,
+  name varchar(256) not null, 
+  status varchar(256) not null,
+  primary key(challengeId)
+);
+
+/* role */
+create table if not exists role (
+  roleId int auto_increment not null,
+  name varchar(256) not null,
+  primary key(roleId)
+);
+
+/* score */
+create table if not exists score (
+  scoreId int auto_increment not null,
+  nbScore int default 0,
+  primary key(scoreId)
+);
+
+/* promo */
+create table if not exists promo (
+  promoId int auto_increment not null,
+  name varchar(256) not null,
+  primary key(promoId)
+);
+
+/* ssh */
+create table if not exists ssh (
+  sshId int auto_increment not null,
+  sshUserName varchar(256) not null,
+  sshIpAdress varchar(256) not null,
+  primary key(sshId)
+);
+
+/* challenge_user */
+create table if not exists challenge_user (
+  challengeId int,
+  userId int,
+  promoId int,
+  scoreId int,
+  foreign key(challengeId) references challenge(challengeId),
+  foreign key(userId) references user(userId),
+  foreign key promoId references promo(promoId),
+  foreign key(scoreId) references score(scoreId)
 );
--- dbms/dev-initdb.d/001-init-dev.sql
+++ dbms/dev-initdb.d/001-init-dev.sql
@@ -3,10 +3,10 @@ Script de création de la base de données de test.
 A noter, on utilise une stratégie avec DROP et IF NOT EXISTS afin de rendre 
 notre script réutilisable dans le future, même si la base existe déjà
 */
-create database IF NOT EXISTS mtdb;
+create database IF NOT EXISTS challenge_project;
 
 /* Créer l'utilisateur API */
 create user IF NOT EXISTS 'api-dev'@'%.%.%.%' identified by 'api-dev-password';
-grant select, update, insert, delete on mtdb.* to 'api-dev'@'%.%.%.%';
+grant select, update, insert, delete on challenge_project.* to 'api-dev'@'%.%.%.%';
 flush privileges;
 

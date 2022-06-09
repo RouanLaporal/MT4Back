c6afbdabaacf693dd51685b235b07eb076abaf95
Rouan <rouan@manuscry.com>
2022-06-09 14:47:45 +0200

    fix cors

--- src/api.ts
+++ src/api.ts
@@ -15,8 +15,19 @@ const PORT = process.env.PORT || 5050;
 const app = Express();
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
 }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
 
 // L'appli parse le corps du message entrant comme du json
 app.use(json());
